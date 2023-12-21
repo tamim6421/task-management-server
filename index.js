9301f799d512a07989db3214d15ad3680b5a0af3
ProgrammingHero <tamimhossain6421@gmail.com>
2023-12-21 13:49:30 +0600

    setup server and connect db

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+const cors = require ('cors')
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const { MongoClient } = require('mongodb');
+
+// middleware 
+app.use(cors())
+app.use( express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iimwc2a.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a new MongoClient
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    
+
+
+
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) =>{
+    res.send('Task Management Server')
+})
+
+app.listen( port, () =>{
+    console.log(`Server is running port ${port}`)
+})
